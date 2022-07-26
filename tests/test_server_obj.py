53e5e9b3aa725ce7d34013fc7699f97193a7bc76
Emily Widjaja <emily.widjaja@oriel.ox.ac.uk>
2022-07-26 11:52:21 +0800

    cleaned up

--- tests/test_server_obj.py
+++ tests/test_server_obj.py
@@ -1,9 +1,5 @@
 import os
 import sys
-import inspect
-# print(os.getcwd())
-# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-# parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, os.getcwd()) 
 
 import pytest
